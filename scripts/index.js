608a7b5c9ccacdf89c02a1a1478464d59bb36cf8
Antonio <antoniodias1106@gmail.com>
2023-11-12 02:04:03 -0300

    Atualização do responsivo

---
+++ scripts/index.js
@@ -0,0 +1,8 @@
+const modalOverlay = document.querySelector('.modalOverlay');
+
+function handleModalOpen(){
+    modalOverlay.style.display = 'flex'
+}
+function handleCloseModal(){
+    modalOverlay.style.display = 'none'
+};
